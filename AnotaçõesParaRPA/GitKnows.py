555e3babb8b1c6c69a33ee0cccab98e8e66a89d0
Joel Paes <jodenis@outlook.com>
2025-02-15 03:25:55 -0300

    Anotações importantes

---
+++ AnotaçõesParaRPA/GitKnows.py
@@ -0,0 +1,4 @@
+# cd ~/OneDrive/Área\ de\ Trabalho/CARREIRA\ PROGRAMAÇÃO/CursoPythonRPA
+# git init
+# esse comando cria um repositório local
+# e o cd é para entrar na pasta, e o git init é para iniciar o repositório
